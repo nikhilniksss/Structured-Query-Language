58eb2a95e2514b61252390c9ebbac7691bfcf7ab
Nikhil Kumar <49320663+nikhilniksss@users.noreply.github.com>
2025-01-30 13:15:22 +0530

    Add files via upload

--- namastesql/day_3.sql
+++ namastesql/day_3.sql
@@ -46,3 +46,51 @@ INSERT INTO Orders (
 "Office Supplies","Binders",
 "Vinyl Sectional Post Binders",
 22.62,2,0.7,-15.08);
+
+-- Alter table
+
+SELECT * FROM amazon_orders;
+
+DESC amazon_orders;
+
+-- Want to alter order_date to capture time as well
+
+ALTER TABLE amazon_orders MODIFY COLUMN order_date DATETIME;
+
+DESC amazon_orders;
+
+-- now order_date will capture time as well
+
+INSERT INTO amazon_orders VALUES (7,'2025-01-28 17:32:00','Cup',300.5,'emi');
+
+SELECT * FROM amazon_orders;
+
+-- Add new column to the existing table 
+
+ALTER TABLE amazon_orders ADD COLUMN user_name VARCHAR(30);
+
+ALTER TABLE amazon_orders ADD COLUMN category VARCHAR(30);
+
+-- Drop column from the table without deleting the table
+
+ALTER TABLE amazon_orders DROP COLUMN category;
+
+DESC amazon_orders;
+
+-- Constraints in SQL is setting up a condition to a column in which it is going to accept the value
+
+CREATE TABLE a_orders(
+    order_id INT NOT NULL,
+    order_date DATE,
+    product_name VARCHAR(30),
+    total_price DECIMAL(6,2),
+    payment_method VARCHAR(20)
+);
+
+INSERT INTO a_orders VALUES(null,'2024-01-28','iphone','9999.20','cash');
+-- 1048 (23000): Column 'order_id' cannot be null
+
+
+
+
+
