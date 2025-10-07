b59ec8984b59ae3c818d344326695d9504202650
Nikhil Kumar <49320663+nikhilniksss@users.noreply.github.com>
2025-10-07 19:57:38 +0530

    Update problem 78 easy.sql

--- Practice/problem 78 easy.sql
+++ Practice/problem 78 easy.sql
@@ -1,5 +1,5 @@
-write an sql query to find the id, name,rating of a movies with odd IDs and whose description is not "boring"
-return the result table ordered by ratings in descending order.
+-- write an sql query to find the id, name,rating of a movies with odd IDs and whose description is not "boring"
+-- return the result table ordered by ratings in descending order.
 
 SELECT id,movie,rating
 FEOM cinema
