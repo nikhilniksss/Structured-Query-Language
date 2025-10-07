2817a7d5717ad88fb3c1dfe560e3182d0ab5d373
Nikhil Kumar <49320663+nikhilniksss@users.noreply.github.com>
2025-10-07 19:57:12 +0530

    Create problem 78 easy.sql

---
+++ Practice/problem 78 easy.sql
@@ -0,0 +1,8 @@
+write an sql query to find the id, name,rating of a movies with odd IDs and whose description is not "boring"
+return the result table ordered by ratings in descending order.
+
+SELECT id,movie,rating
+FEOM cinema
+WHERE description != "boring"
+AND id % 2 != 0
+ORDER BY rating DESC;
